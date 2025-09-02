bf7daf3ec23b4f9c2db614828aea2066f43d6e49
Ole Albers <ole@nocarrier.de>
2025-09-02 21:34:46 +0200

    added structured logging

--- ImapReceiver.cs
+++ ImapReceiver.cs
@@ -23,51 +23,50 @@ namespace AiMailScanner
 
         public async Task<uint?> ReceiveUnCheckedEmails(uint? lastProcessedMailId)
         {
-            using (var client = new ImapClient())
+            using var client = new ImapClient();
+            try
+            {
+                await client.ConnectAsync(_config.ImapConfig.Url, _config.ImapConfig.Port, true);
+                await client.AuthenticateAsync(_config.ImapConfig.UserName, _config.ImapConfig.Password);
+                await client.Inbox.OpenAsync(FolderAccess.ReadOnly);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Cannot connect to Imap server. Check Credentials and configuration. Program will stop");
+                throw;
+            }
+            _logger.LogDebug("Connected to Server '{Url}:{Port}'",_config.ImapConfig.Url, _config.ImapConfig.Port);
+
+            var uids = await client.Inbox.SearchAsync(SearchQuery.SentSince(DateTime.Now.AddDays(-1)));
+            var unprocessedUids = lastProcessedMailId == null ? uids : uids.Where(q => q.Id > lastProcessedMailId);
+            if (!unprocessedUids.Any()) return lastProcessedMailId;
+
+            _logger.LogInformation("Retrieved '{Count}' new emails to process", unprocessedUids.Count());
+
+            foreach (var uid in unprocessedUids)
             {
                 try
                 {
-                    client.Connect(_config.ImapConfig.Url, _config.ImapConfig.Port, true);
-                    client.Authenticate(_config.ImapConfig.UserName, _config.ImapConfig.Password);
-                    client.Inbox.Open(FolderAccess.ReadOnly);
+                    var message = client.Inbox.GetMessage(uid);
+                    var body = message.HtmlBody;
+                    if (string.IsNullOrWhiteSpace(body)) body = message.TextBody;
+                    var summary = await _openAiMailFunctions.GetSummaryFromEmailContent(message.Subject, body);
+                    if (summary != null)
+                    {
+                        var from = message.From.First().ToString();
+                        summary.Subject = message.Subject;
+                        summary.Contact = from;
+                        _sender.AddToCalendarIfNotExisting(summary);
+                    }
+                    Thread.Sleep(TimeSpan.FromSeconds(10));
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex, "Cannot connect to Imap server. Check Credentials and configuration. Program will stop");
-                    throw;
+                    _logger.LogError(ex, "Failed reading email with id '{uid}' from imapserver. Will discard that mail and continue with next one.", uid);
                 }
-
-                var uids = client.Inbox.Search(SearchQuery.SentSince(DateTime.Now.AddDays(-1)));
-                var unprocessedUids = lastProcessedMailId == null ? uids : uids.Where(q => q.Id > lastProcessedMailId);
-                if (!unprocessedUids.Any()) return lastProcessedMailId;
-
-                _logger.LogInformation("Retrieved '{Count}' new emails to process", unprocessedUids.Count());
-
-                foreach (var uid in unprocessedUids)
+                finally
                 {
-                    try
-                    {
-                        var message = client.Inbox.GetMessage(uid);
-                        var body = message.HtmlBody;
-                        if (string.IsNullOrWhiteSpace(body)) body = message.TextBody;
-                        var summary = await _openAiMailFunctions.GetSummaryFromEmailContent(message.Subject, body);
-                        if (summary != null)
-                        {
-                            var from = message.From.First().ToString();
-                            summary.Subject = message.Subject;
-                            summary.Contact = from;
-                            _sender.AddToCalendarIfNotExisting(summary);
-                        }
-                        Thread.Sleep(TimeSpan.FromSeconds(10));
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, "Failed reading email with id '{uid}' from imapserver. Will discard that mail and continue with next one.", uid);
-                    }
-                    finally
-                    {
-                        lastProcessedMailId = uid.Id;
-                    }
+                    lastProcessedMailId = uid.Id;
                 }
             }
 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿using AiMailScanner;
-
+﻿using System.Reflection;
+using AiMailScanner;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
-using NLog.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Grafana.Loki;
 
 internal class Program
 {
@@ -12,12 +15,14 @@ internal class Program
     private static async Task Main()
     {
         Console.WriteLine("Application just Started");
-        var config = Newtonsoft.Json.JsonConvert.DeserializeObject<Config>(File.ReadAllText("secrets.json")) ?? throw new Exception("Cannot read config");
+        var config = Newtonsoft.Json.JsonConvert.DeserializeObject<Config>(File.ReadAllText("secrets.json")) ??
+                     throw new Exception("Cannot read config");
         var services = new ServiceCollection();
         ConfigureServices(services, config);
 
-        using ServiceProvider serviceProvider = services.BuildServiceProvider();
-        var imapReceiver = serviceProvider.GetService<ImapReceiver>() ?? throw new Exception("Failed building services");
+        await using var serviceProvider = services.BuildServiceProvider();
+        var imapReceiver = serviceProvider.GetService<ImapReceiver>() ??
+                           throw new Exception("Failed building services");
         while (true)
         {
             _lastReadId = await imapReceiver.ReceiveUnCheckedEmails(_lastReadId);
@@ -30,11 +35,27 @@ internal class Program
         services.AddScoped<OpenAiMailFunctions>();
         services.AddScoped<DavSender>();
         services.AddScoped<ImapReceiver>();
-        IServiceCollection serviceCollection = services.AddSingleton(config);
-        services.AddLogging(builder =>
+        services.AddSingleton(config);
+        services.AddLogging(cfg => cfg.SetMinimumLevel(LogLevel.Debug));
+        services.AddSerilog(cfg =>
         {
-            builder.SetMinimumLevel(LogLevel.Information);
-            builder.AddNLog("nlog.config");
+            cfg.MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("job", Assembly.GetEntryAssembly()?.GetName().Name)
+                .Enrich.WithProperty("desktop", Environment.GetEnvironmentVariable("DESKTOP_SESSION"))
+                .Enrich.WithProperty("language", Environment.GetEnvironmentVariable("LANGUAGE"))
+                .Enrich.WithProperty("lc", Environment.GetEnvironmentVariable("LC_NAME"))
+                .Enrich.WithProperty("timezone", Environment.GetEnvironmentVariable("TZ"))
+                .Enrich.WithProperty("dotnetVersion", Environment.GetEnvironmentVariable("DOTNET_VERSION"))
+                .Enrich.WithProperty("inContainer", Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"))
+                .WriteTo.GrafanaLoki(Environment.GetEnvironmentVariable("LOKIURL") ?? "http://thebeast:3100",
+                    propertiesAsLabels: ["job"]);
+            if (Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration ==
+                "Debug")
+            {
+                cfg.WriteTo.Console(new RenderedCompactJsonFormatter());
+            }
         });
     }
 }
